684ff6308e9de3eb0b3466498db27f0f3fa1c427
Andrzej Mazur <andrzej@mazur.info>
2021-03-21 19:07:49 +0100

    logging cleaned up in influxdbdatalogger.py script, printout of data sample from DS18B2 added

--- src/influxdbdatalogger.py
+++ src/influxdbdatalogger.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-import logging
-import sys
-import getopt
-import time
-import json
-from datetime import datetime
-import paho.mqtt.client as mqtt
-from influxdb import InfluxDBClient
 
 ###############################################################
 # influxdbdatalogger.py script can be run from command line   #
@@ -17,6 +9,20 @@ from influxdb import InfluxDBClient
 #                          <C> Andrzej Mazur, 17/03/2021      #
 ###############################################################
 
+import logging
+import sys
+import getopt
+import time
+import json
+import os
+from datetime import datetime
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+
+#Set debug to True in order to log all messages!
+LOG_ALL = False
+#Set log_to_file flag to False in order to print logs on stdout
+LOG_TO_FILE = False
 
 ##########################
 #MQTT Connection Settings#
@@ -44,9 +50,6 @@ influxdb_measurementname = "climatemeasurements"
 influxdb_host = "127.0.0.1"
 influxdb_port = 8086
 
-
-debug = False
-
 def cmd_usage():
   print ('Usage: '+sys.argv[0]+' {[-d | --debug debug] [-h | --host host] [-q | --qos QoS] [-t | --topic topic] [-a | --bfe280addr bfe280 address]')
   exit (1)
@@ -57,9 +60,6 @@ try:
                                                              'qos=',
                                                              'topic=',   
                                                              ])
- #   if not options:
- #     print ('No options supplied')
- #     usage()
       
 except getopt.GetoptError as err:
     print(str(err))
@@ -79,33 +79,51 @@ for opt, arg in options:
 
 #configure logger module
 #levels: DEBUG,INFO,WARNING,ERROR,CRITICAL
-if debug==True:
-    logging.basicConfig(level=logging.DEBUG,format='%(asctime)s:%(filename)s:%(lineno)s:%(levelname)s:%(message)s')
+ 
+#create two log file handlers, one for actual log file and another for stdout
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+if LOG_TO_FILE == True:
+    #extract file name from filename.extension
+    idx=os.path.split(os.path.basename(__file__))[1].find('.')
+    file_name_wo_extension=os.path.split(os.path.basename(__file__))[1][:idx]
+    log_file = os.path.dirname(os.path.realpath(__file__)) + "/" + file_name_wo_extension + ".log"
+    file_handler = logging.FileHandler(filename=log_file)
+    hndls = [file_handler]
+    print ("Program logs are stored in: ", log_file)
 else:
-    logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(filename)s:%(lineno)s:%(levelname)s:%(message)s')
-    
+    hndls = [stdout_handler]
+        
+#configure logger module
+#levels: DEBUG,INFO,WARNING,ERROR,CRITICAL
+
+if LOG_ALL == True:
+    logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s:%(threadName)s:%(filename)s:%(lineno)s:%(levelname)s:%(message)s', handlers=hndls)
+else:
+    logging.basicConfig(level = logging.INFO,format = '%(asctime)s:%(threadName)s:%(filename)s:%(lineno)s:%(levelname)s:%(message)s', handlers=hndls)
+
     
 if mqtt_qos != 0 and mqtt_qos != 1:
     mqtt_qos = 1
-    logging.warning('Provided MQTT QoS value is not supported. Default QoS=1 is used...')
+    logging.error('Provided MQTT QoS value is not supported. Default QoS=1 is used...')
 
 def mqtt_on_connect(mqtt_client, userdata, flags, rc):
     if rc==0:
         mqtt.Client.connected_flag=True #flag set
-        logging.debug('Received:MQTT_CONNACK(rc=%i)',rc)
-        logging.debug('Connection to MQTT Broker established!')
+        logging.info('Received:MQTT_CONNACK(rc=%i)',rc)
+        logging.info('Connection to MQTT Broker established!')
         #Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
         (result,mid)=mqtt_client.subscribe(mqtt_topic,mqtt_qos)
-        logging.debug('Sent:MQTT_SUBSCRIBE(mid=%i, topic:%s, QoS=%i, rc=%i)',mid,mqtt_topic, mqtt_qos, result)
+        logging.info('Sent:MQTT_SUBSCRIBE(mid=%i, topic:%s, QoS=%i, rc=%i)',mid,mqtt_topic, mqtt_qos, result)
     else:
-        logging.debug('Received:MQTT_CONNACK(rc=%i)',rc)
-        logging.debug('Connection establishment to MQTT Broker failed!')
+        logging.info('Received:MQTT_CONNACK(rc=%i)',rc)
+        logging.info('Connection establishment to MQTT Broker failed!')
         
 def mqtt_on_disconnect(mqtt_client, userdata, rc):
     mqtt.Client.connected_flag=False #flag set
     if rc==0:
-       logging.debug('Disconnection from MQTT Broker completed!')
+       logging.info('Disconnection from MQTT Broker completed!')
     else:
         logging.error('Unexpected disconnection from MQTT Broker!')
         
@@ -117,24 +135,26 @@ def mqtt_on_message(mqtt_client, userdata, msg):
 
     
 def mqtt_on_subscribe(client,userdata,mid,granted_qos):
-    logging.debug('Received:MQTT_SUBACK(mid=%i,negotiatedQoS=%i)',mid, granted_qos[0])
+    logging.info('Received:MQTT_SUBACK(mid=%i,negotiatedQoS=%i)',mid, granted_qos[0])
     logging.info('Client ready to receive messages!')
 
 def influxdb_store_data_sample(dbhost,dbport,dbuser,dbpass,dbname,dbmeasurement,data):
-    #build database record from received json string
-    logging.info('Measurement to be added to "%s" meas. in "%s" db:',dbmeasurement,dbname)
-    logging.info("   sensor id: %s", str(data['bme280id']))
-    logging.info('   timestamp: %s',list(data.keys())[1])
-    logging.info('   temperature: %f%s',float(data[list(data.keys())[1]]['temperature']['value']),data[list(data.keys())[1]]['temperature']['unit'])
-    logging.info('   pressure: %f%s',float(data[list(data.keys())[1]]['pressure']['value']),data[list(data.keys())[1]]['pressure']['unit'])
-    logging.info('   humidity: %f%s',float(data[list(data.keys())[1]]['humidity']['value']),data[list(data.keys())[1]]['humidity']['unit'])
- 
+    #build database records from received json string
+     
     # connect to influx
     # it is possible to connect once and in this function call just write to database, but it is less reliable method
     ifclient = InfluxDBClient(dbhost,dbport,dbuser,dbpass,dbname)
     
     # write the measurement taken by bme280
     # format the data as a single measurement for influx
+    
+    logging.debug('Measurement to be added to "%s" meas. in "%s" db:',dbmeasurement,dbname)
+    logging.debug("   sensor id: %s", str(data['bme280id']))
+    logging.debug('   timestamp: %s',list(data.keys())[1])
+    logging.debug('   temperature: %f%s',float(data[list(data.keys())[1]]['temperature']['value']),data[list(data.keys())[1]]['temperature']['unit'])
+    logging.debug('   pressure: %f%s',float(data[list(data.keys())[1]]['pressure']['value']),data[list(data.keys())[1]]['pressure']['unit'])
+    logging.debug('   humidity: %f%s',float(data[list(data.keys())[1]]['humidity']['value']),data[list(data.keys())[1]]['humidity']['unit'])
+
     dbrecord = [
         {
             "measurement": dbmeasurement,
@@ -153,8 +173,14 @@ def influxdb_store_data_sample(dbhost,dbport,dbuser,dbpass,dbname,dbmeasurement,
 
     ifclient.write_points(dbrecord)
     
-    # write the measurement taken by bme280
+    # write the measurement taken by ds18b20
     # format the data as a single measurement for influx
+
+    logging.debug('Measurement to be added to "%s" meas. in "%s" db:',dbmeasurement,dbname)
+    logging.debug("   sensor id: %s", str(data['ds18b2id']))
+    logging.debug('   timestamp: %s',list(data.keys())[3])
+    logging.debug('   temperature: %f%s',float(data[list(data.keys())[3]]['temperature']['value']),str(data[list(data.keys())[3]]['temperature']['unit']))
+
     dbrecord = [
         {
             "measurement": dbmeasurement,
@@ -184,7 +210,7 @@ mqtt_client.on_disconnect=mqtt_on_disconnect
 mqtt_client.on_message=mqtt_on_message
 mqtt_client.on_subscribe=mqtt_on_subscribe
 
-logging.debug('Sent:MQTT_CONNECT:(IP:%s,TCP Port:%s,Topic:%s,QoS:%i,KeepAlive:%i)',mqtt_broker_address, mqtt_broker_port, mqtt_topic, mqtt_qos, mqtt_keep_alive)
+logging.info('Sent:MQTT_CONNECT:(IP:%s,TCP Port:%s,Topic:%s,QoS:%i,KeepAlive:%i)',mqtt_broker_address, mqtt_broker_port, mqtt_topic, mqtt_qos, mqtt_keep_alive)
 
 #connect to MQTT Broker
 try:
@@ -198,7 +224,7 @@ try:
     mqtt_client.loop_forever()
 except KeyboardInterrupt:
     logging.info('Exiting the program, ctrl+C pressed...')
-    logging.debug('Sent:MQTT_DISCONNECT')
-    logging.debug('Disconnecting from MQTT Broker')
+    logging.info('Sent:MQTT_DISCONNECT')
+    logging.info('Disconnecting from MQTT Broker')
     mqtt_client.disconnect();
 
